9ddf0439c593d2271b09c312a09d56140557f2a8
vladshkurin <shkurinvlad@gmail.com>
2016-07-20 18:20:15 +0300

    ember tutorial

---
+++ app/components/rental-listing.js
@@ -0,0 +1,11 @@
+export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow() {
+      this.set('isImageShowing', true);
+    },
+    imageHide() {
+      this.set('isImageShowing', false)
+    }
+  }
+});
---
+++ app/controllers/index.js
@@ -0,0 +1,23 @@
+export default Ember.Controller.extend({
+  filteredList: null,
+  actions: {
+    autoComplete(param) {
+      if(param !== "") {
+        this.store.query('rental', {city: param}).then((result) => {
+          this.set('filteredList',result);
+        });
+      } else {
+        this.set('filteredList').clear();
+      }
+    },
+    search(param) {
+      if(param !== "") {
+        this.store.query('rental', {city: param}).then((result) => {
+          this.set('model',result);
+        });
+      } else {
+        this.set('model').clear();
+      }
+    }
+  }
+});
